42ef8d58eaa18af95bd99192bb53218ee168ddfa
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-09-02 19:41:59 +0900

    Develop Board

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,7 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-interface SquaresType {
-  [squares: number]: (string | null)[];
-}
+type SquaresType = (string | null)[];
 
 const calculateWinner = (squares: SquaresType) => {
   const lines = [
@@ -39,37 +37,41 @@ const Square = (props: SquareType) => {
   );
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
+interface BoardProps {
+  squares: SquaresType;
+  onClick: (i: number) => void;
+}
+
+const Board = (props: BoardProps) => {
+  const renderSquare = (i: number) => {
     return (
       <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)}
       />
     );
   }
 
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+
 }
 
 class Game extends React.Component {
