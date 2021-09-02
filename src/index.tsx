41700afcab156fb90ef5559cf9fa5b03acbd7725
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-09-02 19:32:56 +0900

    Develop calculateWinner

--- src/index.tsx
+++ src/index.tsx
@@ -26,17 +26,6 @@ const calculateWinner = (squares: SquaresType) => {
   return null;
 }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
